3170ae89826682ad0013222796883702279802d9
LimHyeonMo <gusah34@gmail.com>
2021-11-19 14:15:56 +0900

    update

---
+++ src/views/accounts/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <div>
+      <label for="username">사용자 이름: </label>
+      <input 
+        type="text" 
+        id="username"
+        v-model="credentials.username"
+      >
+    </div>
+    <div>
+      <label for="password">비밀번호: </label>
+      <input 
+        type="password" 
+        id="password"
+        v-model="credentials.password"
+      >
+    </div>
+    <button>로그인</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Login',
+  data: function () {
+    return {
+      credentials: {
+        username: null,
+        password: null,
+      }
+    }
+  },
+}
+</script>
