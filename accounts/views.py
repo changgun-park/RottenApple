31f446f7c50b80979bc71f2fc1e1651bbd2038ae
박창건 <pcg0527@naver.com>
2021-11-22 09:44:23 +0900

    add changgun 1122

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from .serializers import UserSerializer
+from rest_framework import status
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def signup(request):
+    # Client가 보낸 데이터 중 비밀번호 확인
+    password = request.data.get('password')
+    password_confirmation = request.data.get('passwordConfirmation')
+    if password != password_confirmation:
+        return Response({'error': '비밀번호가 일치하지 않습니다'})
+    
+    genres = request.data.get('selected')
+
+    serializer = UserSerializer(data=request.data)
+    
+    if serializer.is_valid(raise_exception=True):
+        user = serializer.save()
+        # 비밀번호 해싱
+        user.set_password(request.data.get('password'))
+        # DB 반영
+        user.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
--- server/settings.py
+++ server/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     # installed apps
     'django_extensions',
     'rest_framework',
+    'corsheaders',
     # 3rd party apps
     'django.contrib.admin',
     'django.contrib.auth',
@@ -48,6 +49,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    # django cors middleware setting
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,6 +59,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# 모든 origin 허용
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'server.urls'
 
