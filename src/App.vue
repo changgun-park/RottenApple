6ba9ea9274f2fdad78fc912e59d34626338973ad
박창건 <pcg0527@naver.com>
2021-11-22 09:22:19 +0900

    add auth

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,110 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link :to="{ name: 'Signup' }">Signup</router-link> |
-      <router-link :to="{ name: 'Login' }">Login</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+        app
+        clipped-left
+        color="indigo"
+        dark
+      >
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+  
+        <v-toolbar-title>MovieGram</v-toolbar-title>
+  
+        <v-spacer></v-spacer>
+        
+        <v-btn text>Home</v-btn>
+        <v-btn text>Reviews</v-btn>
+        <v-btn text>Community</v-btn>
+        
+        <v-spacer></v-spacer>
+  
+        <v-menu offset-y
+          left
+          bottom
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              Text
+              v-bind="attrs"
+              v-on="on"
+              color="indigo"
+            >
+              <v-icon>mdi-account</v-icon>Anonymous
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+  
+          <v-list>
+            <v-list-item :to="{ name: 'Login' }">
+              <v-list-item-title>Login</v-list-item-title>
+            </v-list-item>
+            <v-list-item :to="{ name: 'Signup' }">
+              <v-list-item-title>Signup</v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title>Profile</v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title>Logout</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+      
+    <v-footer app>
+      <span>2021</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-export default {
-  name: 'App',
-}
+// import HelloWorld from './components/HelloWorld.vue'
+
+  export default {
+    components: { 
+      // HelloWorld 
+    },
+    props: {
+      source: String
+    },
+    data: function () {
+      return {
+        drawer: null,
+        items: [
+          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+          { title: 'Photos', icon: 'mdi-image' },
+          { title: 'About', icon: 'mdi-help-box' },
+        ]
+      } 
+    }  
+  }
 </script>
-
-
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- src/views/accounts/Signup.vue
+++ src/views/accounts/Signup.vue
@@ -26,14 +26,32 @@
         @keyup.enter="signup"
       >
     </div>
+      
+        <multiselect
+          v-model="credentials.selected"
+          :options="options"
+          :multiple="true"
+          :close-on-select="false"
+          :clear-on-select="false"
+          :preserve-search="true"
+          track-by="id"
+          label="name"
+          :max="3"
+          placeholder="">
+            <span slot="maxElements">최대 3개만 선택 할 수 있습니다.</span>
+        </multiselect>
     <button @click="signup">회원가입</button>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import Multiselect from 'vue-multiselect'
 
 export default {
+  components: {
+    Multiselect,
+  },
   name: 'Signup',
   data: function () {
     return {
@@ -41,15 +59,38 @@ export default {
         username: null,
         password: null,
         passwordConfirmation: null,
-      }
+        selected: []
+      },
+      alert: 'maxed out',
+      options: [
+        { id: 12, name: '모험',  },
+        { id: 14, name: '판타지',  },
+        { id: 16, name: '애니메이션',  },
+        { id: 18, name: '드라마',  },
+        { id: 27, name: '공포',  },
+        { id: 28, name: '액션',  },
+        { id: 35, name: '코미디'},
+        { id: 36, name: '역사'},
+        { id: 37, name: '서부'},
+        { id: 53, name: '스릴러'},
+        { id: 80, name: '범죄'},
+        { id: 99, name: '다큐멘터리'},
+        { id: 878, name: 'SF'},
+        { id: 9648, name: '미스터리'},
+        { id: 10402, name: '음악'},
+        { id: 10749, name: '로맨스'},
+        { id: 10751, name: '가족'},
+        { id: 10752, name: '전쟁'},
+        { id: 10770, name: 'TV영화'},
+      ]
     }
   },
   methods: {
     signup: function () {
       axios({
         method: 'post',
-        url: 'http://127.0.0.1:8000/accounts/signup',
-        data: this.credentials
+        url: 'http://127.0.0.1:8000/accounts/signup/',
+        data: this.credentials,
       })
         .then(() => {
           this.$router.push({ name: 'Login' })
@@ -57,11 +98,12 @@ export default {
         .catch(err => {
           console.log(err)
         })
-    }
+    },
   }
 }
 </script>
 
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
 <style>
 
 </style>
