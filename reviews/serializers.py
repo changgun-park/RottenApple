74cc0b037d067b34f02675c59b9838fbe8f915fc
박창건 <pcg0527@naver.com>
2021-11-24 06:33:10 +0900

    update reivews.serializers: format created_at

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -5,6 +5,13 @@ class ReviewListSerializer(serializers.ModelSerializer):
     # 영화 이름 출력
     movie = serializers.StringRelatedField()
     user = serializers.StringRelatedField()
+    # 날짜 포맷 변경
+
+    # 현재 시간을 구하고, 현재 시간과 created_at이 같은 날짜면 포맷을 '시간:분'으로 serialize
+    # 그게 아니라면, '년-월-일'로 serialize 
+    # current = datetime.datetime.now()
+
+    created_at = serializers.DateTimeField(format="%Y-%m-%d")
 
     class Meta:
         model = Review
